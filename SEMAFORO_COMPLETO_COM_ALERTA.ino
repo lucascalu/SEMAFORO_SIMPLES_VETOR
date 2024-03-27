f71c259c4e10b21c59f69fd6b53a893a51f619ac
Lucas Schiavon Calu <54896910+lucascalu@users.noreply.github.com>
2024-03-27 15:42:16 -0300

    Create SEMAFORO_COMPLETO_COM_ALERTA.ino

---
+++ SEMAFORO_COMPLETO_COM_ALERTA.ino
@@ -0,0 +1,246 @@
+int SEM1[3];
+int PED1[2];
+
+int SEM2[3];
+int PED2[2];
+
+void setup()
+{
+  
+  SEM1[0] = 13;
+  SEM1[1] = 12;
+  SEM1[2] = 11;
+
+ PED1[0] = 9;
+ PED1[1] = 8;
+
+
+  SEM2[0] = 5;
+  SEM2[1] = 4;
+  SEM2[2] = 3;
+
+PED2[0] = 1;
+PED2[1] = 0;
+  
+
+  pinMode(SEM1[0], OUTPUT);
+  pinMode(SEM1[1], OUTPUT);
+  pinMode(SEM1[2], OUTPUT);
+
+  pinMode(PED1[0], OUTPUT);
+  pinMode(PED1[1], OUTPUT);
+
+  pinMode(SEM2[0], OUTPUT);
+  pinMode(SEM2[1], OUTPUT);
+  pinMode(SEM2[2], OUTPUT);
+
+  pinMode(PED2[0], OUTPUT);
+  pinMode(PED2[1], OUTPUT);
+
+  
+
+}
+
+
+void loop()
+{
+    
+  
+
+  
+  digitalWrite(SEM1[0], HIGH);
+  digitalWrite(SEM1[1], LOW);  
+  digitalWrite(SEM1[2], LOW);
+  
+  digitalWrite(PED1[0],LOW);
+  digitalWrite(PED1[1],HIGH);
+  
+  digitalWrite(SEM2[0], LOW);
+  digitalWrite(SEM2[1], LOW);  
+  digitalWrite(SEM2[2], HIGH);
+  
+  digitalWrite(PED2[0],HIGH);
+  digitalWrite(PED2[1],LOW);
+  
+delay(1000);
+  
+  
+  digitalWrite(SEM1[0], HIGH);
+  digitalWrite(SEM1[1], LOW);  
+  digitalWrite(SEM1[2], LOW);
+  
+  digitalWrite(PED1[0],LOW);
+  digitalWrite(PED1[1],HIGH);
+  
+  digitalWrite(SEM2[0], LOW);
+  digitalWrite(SEM2[1], HIGH);  
+  digitalWrite(SEM2[2], LOW);
+  
+  digitalWrite(PED2[0],HIGH);
+  digitalWrite(PED2[1],LOW);
+  
+delay(1000);
+  
+  
+  //PISCAR LED
+  
+  //1
+  
+  digitalWrite(SEM1[0], HIGH);
+  digitalWrite(SEM1[1], LOW);  
+  digitalWrite(SEM1[2], LOW);
+  
+  digitalWrite(PED1[0],LOW);
+  digitalWrite(PED1[1],HIGH);
+  
+  digitalWrite(SEM2[0], LOW);
+  digitalWrite(SEM2[1], HIGH);  
+  digitalWrite(SEM2[2], LOW);
+  
+  digitalWrite(PED2[0],HIGH);
+  digitalWrite(PED2[1],LOW);
+  
+delay(500);
+  //1
+  
+  
+  //2
+  digitalWrite(SEM1[0], HIGH);
+  digitalWrite(SEM1[1], LOW);  
+  digitalWrite(SEM1[2], LOW);
+  
+  digitalWrite(PED1[0],LOW);
+  digitalWrite(PED1[1],LOW);
+  
+  digitalWrite(SEM2[0], LOW);
+  digitalWrite(SEM2[1], HIGH);  
+  digitalWrite(SEM2[2], LOW);
+  
+  digitalWrite(PED2[0],HIGH);
+  digitalWrite(PED2[1],LOW);
+delay(500);  
+  //2
+  
+  
+  //3
+   digitalWrite(SEM1[0], HIGH);
+  digitalWrite(SEM1[1], LOW);  
+  digitalWrite(SEM1[2], LOW);
+  
+  digitalWrite(PED1[0],LOW);
+  digitalWrite(PED1[1],HIGH);
+  
+  digitalWrite(SEM2[0], LOW);
+  digitalWrite(SEM2[1], HIGH);  
+  digitalWrite(SEM2[2], LOW);
+  
+  digitalWrite(PED2[0],HIGH);
+  digitalWrite(PED2[1],LOW);
+delay(500);  
+  
+  //3
+  
+  
+  //PISCAR LED
+  
+  
+  digitalWrite(SEM1[0], LOW);
+  digitalWrite(SEM1[1], LOW);  
+  digitalWrite(SEM1[2], HIGH);
+  
+  digitalWrite(PED1[0],HIGH);
+  digitalWrite(PED1[1],LOW);
+  
+  digitalWrite(SEM2[0], HIGH);
+  digitalWrite(SEM2[1], LOW);  
+  digitalWrite(SEM2[2], LOW);
+  
+  digitalWrite(PED2[0],LOW);
+  digitalWrite(PED2[1],HIGH);
+  
+delay(1000);
+  
+  digitalWrite(SEM1[0], LOW);
+  digitalWrite(SEM1[1], HIGH);  
+  digitalWrite(SEM1[2], LOW);
+  
+  digitalWrite(PED1[0],HIGH);
+  digitalWrite(PED1[1],LOW);
+  
+  digitalWrite(SEM2[0], HIGH);
+  digitalWrite(SEM2[1], LOW);  
+  digitalWrite(SEM2[2], LOW);
+  
+  digitalWrite(PED2[0],LOW);
+  digitalWrite(PED2[1],HIGH);
+  
+delay(1000);
+  
+  
+   //PISCAR LED
+  
+  //1
+  digitalWrite(SEM1[0], LOW);
+  digitalWrite(SEM1[1], HIGH);  
+  digitalWrite(SEM1[2], LOW);
+  
+  digitalWrite(PED1[0],HIGH);
+  digitalWrite(PED1[1],LOW);
+  
+  digitalWrite(SEM2[0], HIGH);
+  digitalWrite(SEM2[1], LOW);  
+  digitalWrite(SEM2[2], LOW);
+  
+  digitalWrite(PED2[0],LOW);
+  digitalWrite(PED2[1],HIGH);
+  
+delay(500);
+  
+  //1
+  
+  
+  //2
+  
+  digitalWrite(SEM1[0], LOW);
+  digitalWrite(SEM1[1], HIGH);  
+  digitalWrite(SEM1[2], LOW);
+  
+  digitalWrite(PED1[0],HIGH);
+  digitalWrite(PED1[1],LOW);
+  
+  digitalWrite(SEM2[0], HIGH);
+  digitalWrite(SEM2[1], LOW);  
+  digitalWrite(SEM2[2], LOW);
+  
+  digitalWrite(PED2[0],LOW);
+  digitalWrite(PED2[1],LOW);
+  
+delay(500);
+  //2
+  
+  
+  //3
+  digitalWrite(SEM1[0], LOW);
+  digitalWrite(SEM1[1], HIGH);  
+  digitalWrite(SEM1[2], LOW);
+  
+  digitalWrite(PED1[0],HIGH);
+  digitalWrite(PED1[1],LOW);
+  
+  digitalWrite(SEM2[0], HIGH);
+  digitalWrite(SEM2[1], LOW);  
+  digitalWrite(SEM2[2], LOW);
+  
+  digitalWrite(PED2[0],LOW);
+  digitalWrite(PED2[1],HIGH);
+  
+delay(500);
+  
+  //3
+  
+  
+  //PISCAR LED
+delay(1000);
+  
+  
+}
